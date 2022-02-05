49aa26bb97758b03e18d7f18a791f3708243a4bd
nantech19 <nantech19@gmail.com>
2022-02-05 12:44:42 +0000

    Day1 : Intial commit

---
+++ src/main/java/com/nandu/pro/javapractice1/Javapractice1Application.java
@@ -0,0 +1,13 @@
+package com.nandu.pro.javapractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javapractice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javapractice1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nandu/pro/javapractice1/Javapractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandu.pro.javapractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javapractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
