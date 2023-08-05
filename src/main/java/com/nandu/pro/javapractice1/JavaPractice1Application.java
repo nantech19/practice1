1bc566ce020cef9b8d2a026f85d99782b6fe3233
nandu_prajapati <nantech19@gmail.com>
2023-08-05 14:30:01 +0100

    Added a MoveElementToEnd Method

--- src/main/java/com/nandu/pro/javapractice1/JavaPractice1Application.java
+++ src/main/java/com/nandu/pro/javapractice1/JavaPractice1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Javapractice1Application {
+public class JavaPractice1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Javapractice1Application.class, args);
+		SpringApplication.run(JavaPractice1Application.class, args);
 	}
 
 }
